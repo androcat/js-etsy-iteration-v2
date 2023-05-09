79225ab1f728958018abd4ca24428b6211913347
Jay Medina <jay.medina.celis@gmail.com>
2023-05-09 14:53:38 -0400

    Defines calcMean function

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
 (function () {
   "use strict";
+
+  function calcMean(arr) {
+    // Here is the sum of all prices
+    // Targets the price key of each obj in the array
+    return (
+      Math.round(
+        (arr.reduce((acc, obj) => acc + obj.price, 0) / arr.length) * 100
+      ) / 100
+    );
+    // for rounding to 2 decimals:
+    // Math.round(num * 100) / 100;
+    // Why ? because via * and then / we reformat via the '00'
+  }
+
+  console.log(`The average price is ${calcMean(items)}`);
 })();
